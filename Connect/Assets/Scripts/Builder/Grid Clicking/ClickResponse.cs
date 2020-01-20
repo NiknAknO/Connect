7ff94211e8265b335af3baa799bec4f7b18e23fe
Niko Rush <familiarush@hotmail.com>
2020-01-20 17:10:56 -0500

    Fixed build mode scripts file and corrected zooming issue.
    Since .gitignore ignores folders named "Build," I renamed the build mode scripts folder to "Builder." Also, I fixed an issue where the camera zoomed into the wrong spot in build mode when moving the camera around.

---
+++ Connect/Assets/Scripts/Builder/Grid Clicking/ClickResponse.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickResponse : MonoBehaviour
+{
+    public void whenClicked()
+    {
+        RaycastHit2D hit;
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (hit = Physics2D.Raycast(ray.origin, new Vector2(0, 0)))
+        {
+            Debug.Log(hit.collider.name);
+        }
+    }
+}
