dc7fa7f2886bbd3c52cda260ccdae32ecb77e6e1
Niko Rush <familiarush@hotmail.com>
2020-08-23 00:10:40 -0400

    Overhaul

---
+++ Connect/Assets/Scripts/UI/Menu/MenuUI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuUI : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Build()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Quit() 
+    {
+        Application.Quit();
+    }
+
+    void Start() {
+        Screen.SetResolution(2560, 1440, true);
+    }
+}
